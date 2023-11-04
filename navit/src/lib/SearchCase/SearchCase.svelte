45f9df7fb6b087163b84ffb810ef100573ee785e
Vinicius <59659214+ZeAndradee@users.noreply.github.com>
2023-11-04 18:50:13 -0300

    sidebar concluida + seachbox
    Searchbox com outline azul

--- navit/src/lib/SearchCase/SearchCase.svelte
+++ navit/src/lib/SearchCase/SearchCase.svelte
@@ -1,58 +1,31 @@
-<script>
-
-</script>
-
-<body>
-    <div class="container">
-        <div class="perfil">
-            <img src="E:\Documentos\GitHub\Navit\navit\static\NavitIcons\Header\Perfil.png" alt="">
-        </div>
-    </div>
-    <div class="search-box"> 
-        <input type="text" class="search-text" placeholder="Pesquise aqui">
-        <img class="Search" src="../../static/NavitIcons/Header/Search.png" alt="lupa">
-      </div>
-</body>
+<div class="container">
+    <form action="" class="search-bar">
+        <input type="text" placeholder="Pesquise Aqui" name="q">
+        <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path d="M21 21L16.514 16.506L21 21ZM19 10.5C19 12.7543 18.1045 14.9163 16.5104 16.5104C14.9163 18.1045 12.7543 19 10.5 19C8.24566 19 6.08365 18.1045 4.48959 16.5104C2.89553 14.9163 2 12.7543 2 10.5C2 8.24566 2.89553 6.08365 4.48959 4.48959C6.08365 2.89553 8.24566 2 10.5 2C12.7543 2 14.9163 2.89553 16.5104 4.48959C18.1045 6.08365 19 8.24566 19 10.5V10.5Z" stroke="#656575" stroke-width="1.80206" stroke-linecap="round"/>
+          </svg> </button>
+    </form>
+</div>
 
 <style>
-    body{
-        margin: 0;
-        height: 100vh;
-        width: 100vw;
-        background: #F4F5F7;
-    }
-    .search-box{
-        position: absolute;
-        top: 4%;
-        right: 16.875%;
-        
-        background: #FFFFFF;
-        height: 48px;
-        border-radius: 10px;
-        width: 585px;
+    .search-bar{
         display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-    .search-text {
-        border: none;
-        background: none;
+        width: 585px;
+        height: 48px;
+        margin-top: 28px;
+        padding: 10px 24px 12px 32px;
+        background: white;
+        border-radius: 10px;
         outline: none;
-        padding: 12px 24px 12px 32px;
-        font-size: 16px;
-        line-height: 24px;
-        color: #A3A3A3;
-        
     }
-    .Search {
-        padding-right: 24px;
+
+    .search-bar input{
+        background: transparent;
+        flex: 1;
+        border: 0;
+        font-family: "Inter", Arial, sans-serif;
     }
-    .container {
-        position: absolute;
-        top: 2.8889%;
-        right: 2.8472%;
-        
-    }
+    
+    .search-bar input:focus { outline: none; }
+
 </style>
-
-
--- navit/src/routes/Terapeuta/TerapeutaHome/+page.svelte
+++ navit/src/routes/Terapeuta/TerapeutaHome/+page.svelte
@@ -1,21 +1,32 @@
 <script>
-    import EstatisticasDiarias from "$lib/EstatisticasDiarias/EstatisticasDiarias.svelte";
     import SearchCase from "$lib/SearchCase/SearchCase.svelte";
     import SideBarMenuInicio from "$lib/SideBarMenu/SideBarMenuInicio.svelte";
     import GraficoRadar from "$lib/GraficoRadar/PosicionarGrafico.svelte"
 </script>
 
-<div class="flex flex-row h-screen bg-background"  >
     <div>
         <SideBarMenuInicio />
     </div>
     
-    <div> 
+    <div class="search-Box"> 
         <SearchCase />
     </div>
 
-</div>
-<div>
-    <GraficoRadar/>
-</div>
+    <div>
+        <GraficoRadar/>
+    </div>
 
+<style>
+   .search-Box{
+            position: fixed;
+            top: 0;
+            left: 50%;
+            transform: translateX(-50%);
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            
+        }
+            
+</style>
