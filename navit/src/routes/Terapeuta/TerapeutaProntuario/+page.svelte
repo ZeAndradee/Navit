a7769ddaef249d1ac67427cfbdd0da1ea7376cca
Fernanda Hiarita <nandahiarita.15@gmail.com>
2023-11-15 12:51:15 -0300

    pequenas alterações

--- navit/src/routes/Terapeuta/TerapeutaProntuario/+page.svelte
+++ navit/src/routes/Terapeuta/TerapeutaProntuario/+page.svelte
@@ -1,5 +1,24 @@
 <script>
     import SideBarMenuProntuario from "$lib/SideBarMenu/SideBarMenuProntuario.svelte";
+    import SearchCase from "$lib/SearchCase/SearchCase.svelte";
 </script>
 
-<SideBarMenuProntuario />
+<header>
+    <SearchCase/>
+</header>
+
+<div class="sidebar">
+    <SideBarMenuProntuario />
+</div>
+
+<slot>
+<div class="container">
+
+</div>
+</slot>
+<style>
+.container{
+    background-color: aqua;
+}
+</style>
+    
