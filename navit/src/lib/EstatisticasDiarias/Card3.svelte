ea7153597ed7d1f4f8d0e65217d57f3b99a0f33a
Vinicius <59659214+ZeAndradee@users.noreply.github.com>
2023-11-15 21:02:59 -0300

    Estatisticas diarias finalizado

--- navit/src/lib/EstatisticasDiarias/Card3.svelte
+++ navit/src/lib/EstatisticasDiarias/Card3.svelte
@@ -32,8 +32,9 @@ import { onMount } from 'svelte';
           type: 'doughnut',
           data: data,
           options: {
-            cutout: 45,
-            responsive: false, // Define o tamanho do espaço central vazio
+            cutout: 50,
+            responsive: false,
+            rotation: -35// Define o tamanho do espaço central vazio
         },
         });
       }
@@ -43,5 +44,85 @@ import { onMount } from 'svelte';
 
 <!-- Renderizar o elemento canvas -->
 <div class="card3">
-    <canvas bind:this={canvas} width="146px" height="146px"/>
+    <div class="txt-header-card"> 
+      <span style="font-size: 18px; font-weight:500;">Status Pacientes</span>
+      <div class="box-hoje">
+        <span style="font-size: 14px; font-weight: 400; color: #656575">Hoje</span>
+      </div>
+    </div>
+    <!--Grafico-->
+    <div class="grafico-status-pacientes"><canvas bind:this={canvas} width="155px" height="155px"/></div>
+    <div class="legenda">
+      <div class="leg1"><div class="bola"></div> <span style="margin-left: 12px; color: black">Novo</span> <span style="margin-left: auto; color: black">{NovoSP}% <span style="color: #52C93F; font-weight:700">↑</span></span></div>
+      <div class="leg1"><div class="bola" style="background-color: #006AFF;"></div> <span style="margin-left: 12px; color: black">Andamento</span> <span style="margin-left: auto; color: black">{AndamentoSP}% <span style="color: #52C93F; font-weight:700">↑</span></span></div>
+      <div class="leg1"><div class="bola" style="background-color: #FF2727;"></div> <span style="margin-left: 12px; color: black">Finalizado</span> <span style="margin-left: auto; color: black">{FinalizadoSP}% <span style="color: #FF2727; font-weight:700">↓</span></span></div>
+      
+    </div>
+    
 </div>
+
+<style>
+  .card3{
+    display: flex;
+    width: 288px;
+    height: 360px;
+    margin-left: 8%;
+    padding: 24px 24px 20px 22px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 6px;
+    border-radius: 10px;
+    background: #FDFDFD;
+    
+  }
+  .grafico-status-pacientes{
+    width: 100%;
+    display: flex;
+    position: relative;
+    justify-content: center;
+  }
+
+  .legenda{
+    margin-top: 24px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 8px;
+  }
+
+  .leg1{
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    
+  }
+
+  .bola{
+    height: 16px;
+    width: 16px;
+    background-color: #52C93F;
+    border-radius: 50%;
+  }
+  .txt-header-card{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+    padding-bottom: 12px;
+  }
+
+  .box-hoje{
+    width: 52px;
+    height: 24px;
+    border-radius: 2px;
+    padding: 4px 8px;
+    background: #F4F5F7;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
