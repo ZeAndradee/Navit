61b55603e7953d44644291c7d95981d0ede99662
Vinicius <59659214+ZeAndradee@users.noreply.github.com>
2023-11-16 22:21:56 -0300

    Mudanca de diretorio

--- navit/src/routes/Terapeuta/TerapeutaProntuario/+page.svelte
+++ navit/src/routes/Terapeuta/TerapeutaProntuario/+page.svelte
@@ -1,14 +1,16 @@
 <script>
-    import Header from "../../../lib/header/header.svelte";
-    import Headerpaciente from "../../../lib/headerPaciente/headerpaciente.svelte";
-    import InfoPaciente from "../../../lib/infoPaciente/infoPaciente.svelte";
-    import Cardtemptela from "../../../lib/cardtempotela/cardtemptela.svelte";
-    import Cardbotaoutilizado from "../../../lib/cardbotaoutilizado/cardbotaoutilizado.svelte";
+    import Header from "../../../lib/TerapeutaHome/header/header.svelte";
+    import Headerpaciente from "../../../lib/Terapeuta-Prontuarios1/headerPaciente/headerpaciente.svelte";
+    import InfoPaciente from "../../../lib/Terapeuta-Prontuarios1/infoPaciente/infoPaciente.svelte";
+    import Cardtemptela from "../../../lib/Terapeuta-Prontuarios1/cardtempotela/cardtemptela.svelte";
+    import Cardbotaoutilizado from "../../../lib/Terapeuta-Prontuarios1/cardbotaoutilizado/cardbotaoutilizado.svelte";
 </script>
 
 
-<slot>
+<Header/>
+<Headerpaciente/>
+<InfoPaciente/>
+<Cardtemptela/>
+<Cardbotaoutilizado/>
 
-
-</slot>
     
--- navit/src/routes/TestCliques/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    import CliquesRegistrados from "$lib/CliquesRegistrados/CliquesRegistrados.svelte";
-</script>
-<div>
- <CliquesRegistrados/>
-</div>
