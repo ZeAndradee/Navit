930163e5b13257f599a59f802f0d462a66dfce61
Vinicius <59659214+ZeAndradee@users.noreply.github.com>
2023-11-16 23:16:05 -0300

    Att Diretorios

--- navit/src/routes/Terapeuta/TerapeutaProntuario/+page.svelte
+++ navit/src/routes/Terapeuta/TerapeutaProntuario/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-    import Header from "../../../lib/TerapeutaHome/header/header.svelte";
-    import Headerpaciente from "../../../lib/Terapeuta-Prontuarios1/headerPaciente/headerpaciente.svelte";
-    import InfoPaciente from "../../../lib/Terapeuta-Prontuarios1/infoPaciente/infoPaciente.svelte";
-    import Cardtemptela from "../../../lib/Terapeuta-Prontuarios1/cardtempotela/cardtemptela.svelte";
-    import Cardbotaoutilizado from "../../../lib/Terapeuta-Prontuarios1/cardbotaoutilizado/cardbotaoutilizado.svelte";
+    import Header from "../../../lib/Terapeuta/TerapeutaHome/header/header.svelte";
+    import Headerpaciente from "../../../lib/Terapeuta/Terapeuta-Prontuarios2/headerPaciente/headerpaciente.svelte";
+    import InfoPaciente from "../../../lib/Terapeuta/Terapeuta-Prontuarios2/infoPaciente/infoPaciente.svelte";
+    import Cardtemptela from "../../../lib/Terapeuta/Terapeuta-Prontuarios2/cardtempotela/cardtemptela.svelte";
+    import Cardbotaoutilizado from "../../../lib/Terapeuta/Terapeuta-Prontuarios2/cardbotaoutilizado/cardbotaoutilizado.svelte";
 </script>
 
 
