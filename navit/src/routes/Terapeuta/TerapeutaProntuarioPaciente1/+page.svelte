b22dc80831373a67c48c968bd10ba36dbd553273
Arthur Renê <arthurkau12@hotmail.com>
2023-11-22 17:56:44 -0300

    commit de infopaciente

--- navit/src/routes/Terapeuta/TerapeutaProntuarioPaciente1/+page.svelte
+++ navit/src/routes/Terapeuta/TerapeutaProntuarioPaciente1/+page.svelte
@@ -1,5 +1,13 @@
 <script>
     import HeaderProntuario from "$lib/Terapeuta/Terapeuta-Prontuarios1/headerProntuarioPaciente/HeaderProntuario.svelte";
+    import InfoPaciente from "$lib/Terapeuta/Terapeuta-Prontuarios1/Infopaciente/Infopaciente.svelte";
 </script>
+<div>
+    <HeaderProntuario/>   
+</div>
+<div>
+    <InfoPaciente/> 
+</div>
 
-<HeaderProntuario/>
+
+
