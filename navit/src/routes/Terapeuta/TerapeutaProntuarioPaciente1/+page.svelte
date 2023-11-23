58ba144ef6bfc7d6590e594a4d623b74f0dfa5ca
Arthur Renê <arthurkau12@hotmail.com>
2023-11-23 16:52:38 -0300

    tela de infopaciente
    Adicionei a tela de informações do paciente para a aba de prontuários, infelizmente eu não consigo espaçar certinho pq ainda faltam partes da tela.

--- navit/src/routes/Terapeuta/TerapeutaProntuarioPaciente1/+page.svelte
+++ navit/src/routes/Terapeuta/TerapeutaProntuarioPaciente1/+page.svelte
@@ -1,5 +1,18 @@
 <script>
     import HeaderProntuario from "$lib/Terapeuta/Terapeuta-Prontuarios1/headerProntuarioPaciente/HeaderProntuario.svelte";
+    import InfoPaciente from "$lib/Terapeuta/Terapeuta-Prontuarios1/infoPaciente/infoPaciente.svelte"
 </script>
+<div>
+    <HeaderProntuario/>   
+</div>
+<div class="pacienteInfo">
+    <InfoPaciente/> 
+</div>
 
-<HeaderProntuario/>
+<style>
+    .pacienteInfo{
+        float: right;
+        margin-right: 300px;
+        margin-top: 100px;
+    }
+</style>
