1003248afcf6e4ac272525e102d7a0c6bab3d16f
Vinicius <59659214+ZeAndradee@users.noreply.github.com>
2023-11-27 17:02:25 -0300

    Att

---
+++ navit/src/lib/Terapeuta/Configuracoes/DuvidasConta.svelte
@@ -0,0 +1,109 @@
+<script>
+</script>
+
+<div class="configuracoes-conta-card">
+  <div class="title-1">
+    <span>Dúvidas</span>
+  </div>
+  <!-- Divide Line -->
+  <div class="divide-line"></div>
+  <div class="column-1">
+    <div class="comoAjudar-title">
+        <span>Como podemos ajudar?</span>
+    </div>
+    <div class="container">
+        <form action="" class="search-bar">
+            <input type="text" placeholder="Descreva o seu problema" name="q">
+            <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path d="M21 21L16.514 16.506L21 21ZM19 10.5C19 12.7543 18.1045 14.9163 16.5104 16.5104C14.9163 18.1045 12.7543 19 10.5 19C8.24566 19 6.08365 18.1045 4.48959 16.5104C2.89553 14.9163 2 12.7543 2 10.5C2 8.24566 2.89553 6.08365 4.48959 4.48959C6.08365 2.89553 8.24566 2 10.5 2C12.7543 2 14.9163 2.89553 16.5104 4.48959C18.1045 6.08365 19 8.24566 19 10.5V10.5Z" stroke="#656575" stroke-width="1.80206" stroke-linecap="round"/>
+              </svg> </button>
+        </form>
+    </div>
+    <div class="naoencontrou-title">
+        <span>Não encontrou o que buscava? Acesse a nossa <a href="/Terapeuta/Duvidas"><b >Central de Ajuda ao Usuário<b></b></a> </span>
+    </div>
+  </div>
+</div>
+
+<style>
+    .configuracoes-conta-card {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        padding: 40px 70px 30px 70px;
+        border-radius: 10px;
+    }
+    .title-1 {
+        display: flex;
+        flex-direction: row;
+        font-size: 20px;
+        font-weight: 500;
+        justify-content: center;
+    }
+    .divide-line {
+        width: 100%;
+        height: 2px;
+        background: #e0e4ec;
+        margin-top: 40px;
+    }
+
+    .column-1 {
+        display: flex;
+        width: 100%;
+        height: 100%;
+        flex-direction: column;
+        margin-top: 20px;
+        justify-content: center;
+    }
+
+    .comoAjudar-title{
+        display: flex;
+        flex-direction: row;
+        font-size: 26px;
+        font-weight: 500;
+        justify-content: center;
+        margin-top: 20px;
+        color: #006AFF;
+    }
+
+    .search-bar{
+        margin-top: 45px;
+        display: flex;
+        width: 100%;
+        min-width: 319px;
+        max-width: 585px;
+        height: 48px;
+        padding: 10px 24px 12px 32px;
+        background: white;
+        border-radius: 10px;
+        outline: none;
+        margin-left: auto;
+        margin-right: auto;
+        box-shadow: 0px 2.70309px 27.03093px 0px rgba(69, 69, 80, 0.10);
+    }
+
+    
+    .search-bar input{
+        background: transparent;
+        flex: 1;
+        border: 0;
+        font-family: "Inter", Arial, sans-serif;
+    }
+    
+    .search-bar input:focus { outline: none; }
+    
+    .naoencontrou-title{
+        display: flex;
+        margin-top: 78px;
+        justify-content: center;
+        flex-direction: row;
+        font-style: normal;
+        font-weight: 500;
+    }
+
+    .naoencontrou-title b{
+        font-weight: 500;
+        color: #006AFF;
+    }
+</style>
---
+++ navit/src/lib/Terapeuta/Configuracoes/NotificacoesConta.svelte
@@ -0,0 +1,286 @@
+<script>
+</script>
+
+<div class="configuracoes-conta-card">
+  <div class="title-1">
+    <span>Escolher quando e como você quer receber notificações</span>
+  </div>
+  <div class="title-2">
+    <span
+      >Selecione as notificações push e por e-mail que você quer receber</span
+    >
+  </div>
+  <!-- Divide Line -->
+  <div class="divide-line"></div>
+
+  <div class="input-section">
+    <div class="row">
+      <div class="title-3">
+        <span>Geral</span>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="title-4">
+        <span>Gerenciar suas notificações para computadores</span>
+      </div>
+    </div>
+
+    <!-- Opcoes Notificacao -->
+    <div class="column-1">
+        <div class="opcoes">
+            <!-- Opcao 1 -->
+            <div class="row3">
+                <div class="title-5">
+                <span>Notificações no desktop</span>
+                </div>
+                    <div class="row4"  style="margin-left: 20px;">
+                        <div class="column-2">
+                            <!-- Rounded switch -->
+                            <label class="switch">
+                                <input type="checkbox">
+                                <span class="slider round"></span>
+                            </label>
+                        </div>
+                        <div class="column-2">
+                            <span class="bg-title">Receber Notificações no Desktop</span>
+                            <span class="sb-title">Receber notificações mesmo que você não esteja no YouMe</span>
+                        </div>
+                    </div>
+            </div>
+            <div class="row3">
+                <div class="title-5">
+                    <span>Suas Preferências</span>
+                </div>
+                    <div class="opcoes2">
+                        <!-- Opcao 1 -->
+                        <div class="row4">
+                            <div class="column-2">
+                                <!-- Rounded switch -->
+                                <label class="switch">
+                                    <input type="checkbox">
+                                    <span class="slider round"></span>
+                                </label>
+                            </div>
+                            <div class="column-2">
+                                <span class="bg-title">Novos Pacientes</span>
+                                <span class="sb-title">Enviar notificações sobre a atividade de novos pacientes</span>
+                            </div>
+                        </div>
+
+                        <!-- Opcao 2 -->
+                        <div class="row4">
+                            <div class="column-2">
+                                <!-- Rounded switch -->
+                                <label class="switch">
+                                    <input type="checkbox">
+                                    <span class="slider round"></span>
+                                </label>
+                            </div>
+                            <div class="column-2">
+                                <span class="bg-title">Novas Consultas</span>
+                                <span class="sb-title">Enviar notificações sobre a atividade de novas consultas</span>
+                            </div>
+                        </div>
+                    </div>    
+            </div>
+        </div>
+    </div>
+  </div>
+  <div class="row3-btn">
+    <div>
+      <button class="btn-save-profile">Salvar Alterações</button>
+    </div>
+  </div>
+</div>
+
+<style>
+  .configuracoes-conta-card {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    padding: 40px 140px 30px 70px;
+    border-radius: 10px;
+  }
+  .title-1 {
+    font-size: 20px;
+    font-weight: 500;
+  }
+  .title-2 {
+    margin-top: 5px;
+    font-size: 16px;
+    font-weight: 500;
+  }
+
+  .title-2 span {
+    text-align: justify;
+  }
+  .title-3 {
+    margin-top: 15px;
+    font-size: 20px;
+    font-weight: 500;
+  }
+  .title-4 {
+    margin-top: 15px;
+    font-size: 14px;
+  }
+
+  .title-5 {
+    display: flex;
+    flex-direction: row;
+    font-size: 16px;
+    font-weight: 500;
+  }
+
+  .bg-title {
+    font-size: 16px;
+    font-weight: 500;
+  }
+  .sb-title {
+    font-size: 14px;
+    font-weight: 400;
+  }
+
+  .divide-line {
+    width: 100%;
+    height: 2px;
+    background: #e0e4ec;
+    margin-top: 40px;
+  }
+
+  .input-section {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .row {
+    display: flex;
+    flex-direction: row;
+  }
+  .row3 {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    flex-wrap: wrap;
+  }
+
+  .row3 span {
+    margin-left: 23px;
+  }
+
+  .row4{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: center;
+  }
+
+  .opcoes {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+  .opcoes2 {
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    gap: 15px;
+  }
+
+  .column-1 {
+    display: flex;
+    flex-direction: column;
+    margin-top: 40px;
+  }
+  .column-2 {
+    display: flex;
+    flex-direction: column;
+    margin-top: 10px;
+  }
+
+  .btn-save-profile {
+    width: 201px;
+    height: 49px;
+    border-radius: 8px;
+    background: #006aff;
+    color: white;
+    font-weight: 500;
+  }
+
+  .row3-btn {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-top: 40px;
+    gap: 50px;
+  }
+
+  /* Estilo Switch */
+  /* The switch - the box around the slider */
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 85px;
+  height: 34px;
+  transform: scale(0.7);
+}
+
+
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #006AFF;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #006AFF;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
