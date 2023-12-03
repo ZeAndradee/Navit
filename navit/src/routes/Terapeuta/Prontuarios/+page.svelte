e713209872547fcc8e28dd01f691bbabcdf97235
Vinicius <59659214+ZeAndradee@users.noreply.github.com>
2023-12-03 18:04:50 -0300

    Update +page.svelte

--- navit/src/routes/Terapeuta/Prontuarios/+page.svelte
+++ navit/src/routes/Terapeuta/Prontuarios/+page.svelte
@@ -1,9 +1,10 @@
 <script>
-    import SideBarMenuInicio from "$lib/SideBarMenu/SideBarMenu.svelte"
+    import Header from "$lib/Terapeuta/TerapeutaHome/Header/header.svelte";
+    import SideBarMenuInicio from "$lib/SideBarMenu/SideBarMenu.svelte";
     import { onMount } from "svelte";
 
     onMount(() => {
-    document.title = 'Prontuários Pacientes';
+    document.title = 'Prontuários - YouMe';
   });
 </script>
 
@@ -14,27 +15,54 @@
     <div class="sidebarr">
         <SideBarMenuInicio selectedButton="prontuario" />
     </div>
+    <div class="column">
+            <div class="header">
+                <div class="search-case">
+                    <Header />
+                </div>
+            </div>    
+    </div>
+
 </div>
 
 <style>
+    /* Inicio Elementos Padrão */
     .row{
         display: flex;
         flex-direction: row;
         height: 100%;
+        }
+
+    .column{
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
     }
 
     .sidebar{
-    position: fixed;
-    display: flex;
-    height: 100vh;
-    margin-right: 10px;
-        
-    }   
-    
+        display: flex;
+        position: fixed;
+        height: 100vh;
+    }
     .sidebarr{ /*Apenas para dar o espacamento*/
         display: flex;
         height: 100vh;
         margin-right: 10px;
-        
     }
+
+    .header{
+        position: relative;
+        display: flex;
+        margin-left: 28px;
+        margin-top: 28px;
+        flex-direction: row;
+        margin-top: 28px;
+    }
+    .search-case{
+        position: relative;
+        width: 100%;
+        margin-left: auto;
+    }
+/* Fim Elementos Padrão*/
 </style>
