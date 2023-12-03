6eb2360645f49741ceccf20d527a9259e24b7699
Fernanda Hiarita <nandahiarita.15@gmail.com>
2023-12-03 17:48:53 -0300

    tela prontuario em desenvolvimento

---
+++ navit/src/lib/Terapeuta/Terapeuta-Prontuarios2/infoPaci/infopaci.svelte
@@ -0,0 +1,207 @@
+
+
+<body>
+    <div class="container">
+      <p class="id">ID</p>
+      <p class="num-id">q7h3dauolwft7qyi3kb5bbhn</p>
+      <p class = "nascimento">Data de nascimento</p>
+      <p class = "data-nasci">12/05/2004</p>
+      <p class="gen">Gênero</p>
+      <p class="fem">Feminino</p>
+      <p class="random">Data Qualquer</p>
+      <p class="randomdata">07/03/2023</p>
+      <p class="primsessao">Primeira Sessão</p>
+      <p class="primsessao-data">15/05/2023</p>
+      <p class="ultsessao">Ultima Sessão</p>
+      <p class="ultsessao-data">20/10/2023</p>
+    </div>
+  </body>
+  
+  
+  <style>
+    @import url('https://rsms.me/inter/inter.css');
+    .container {
+      position: flex;
+      width: 618px;
+      height: 162px;
+      flex-shrink: 0;
+      background-color: #FFFFFF;
+      border-radius: 10px;
+      align-items: center;
+      padding: 20px;
+    }
+  
+    .id {
+      position: absolute;
+      width: 147px;
+      height: 12px;
+      justify-content: center;
+      flex-shrink: 0;
+      color: #4B4B4B;
+      font-family: Inter;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      margin-left: 18px;
+    }
+  
+    .num-id {
+      position: absolute;
+      width: 193px;
+      height: 17px;
+      color: #000;
+      font-family: 'Inter';
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      margin-top: 40px;
+      margin-left: 18px;
+  
+    }
+  
+    .nascimento {
+      position: absolute;
+      width: 146.203px;
+      height: 12px;
+      justify-content: start;
+      color: #4B4B4B;
+      font-family: 'Inter';
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      flex-shrink: 0;
+      margin-left: 18px;
+      margin-top: 75px;
+    }
+  
+    .data-nasci {
+      position: absolute;
+      width: 79px;
+      height: 17px;
+      color: #000;
+      font-family: 'Inter';
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      margin-top: 100px;
+      margin-left: 18px;
+    }
+  
+    .gen {
+      position: absolute;
+      width: 147px;
+      height: 12px;
+      flex-direction: column;
+      flex-shrink: 0;
+      color: #4B4B4B;
+      font-family: 'Inter';
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      margin-left: 268px;
+    }
+  
+    .fem {
+      position: absolute;
+      width: 70px;
+      height: 17px;
+      color: #000;
+      font-family: Inter;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      margin-top: 40px;
+      margin-left: 268px;
+    }
+  
+    .random {
+      position: absolute;
+      width: 147px;
+      height: 12px;
+      flex-shrink: 0;
+      color: #4B4B4B;
+      font-family: Inter;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      margin-top: 75px;
+      margin-left: 268px;
+    }
+  
+    .randomdata {
+      position: absolute;
+      color: #000;
+      font-family: Inter;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      margin-top: 100px;
+      margin-left: 268px;
+    }
+  
+    .primsessao {
+      position: absolute;
+      width: 147px;
+      height: 12px;
+      flex-direction: column;
+      justify-content: center;
+      flex-shrink: 0;
+      color: #4B4B4B;
+      font-family: Inter;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      margin-left: 450px;
+    }
+  
+    .primsessao-data{
+      position: absolute;
+      color: #000;
+      font-family: Inter;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      margin-left: 450px;
+      margin-top: 40px;
+    }
+  
+    .ultsessao{
+      position: absolute;
+      width: 147px;
+      height: 12px;
+      flex-direction: column;
+      justify-content: center;
+      flex-shrink: 0;
+      color: #4B4B4B;
+      font-family: Inter;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      margin-left: 450px;
+      margin-top: 75px;
+    }
+  
+    .ultsessao-data{
+      position: absolute;
+      color: #000;
+      font-family: Inter;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      margin-left: 450px;
+      margin-top: 100px;
+    }
+    
+  </style>
