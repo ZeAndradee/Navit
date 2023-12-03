ba05dc2a85d95b8cda762ccf38dd558200c3d96c
Vinicius <59659214+ZeAndradee@users.noreply.github.com>
2023-12-02 21:44:57 -0300

    Profile Card
    Em desenvolvimento

--- navit/src/lib/Terapeuta/TerapeutaHome/Header/header.svelte
+++ navit/src/lib/Terapeuta/TerapeutaHome/Header/header.svelte
@@ -1,7 +1,22 @@
+<script>
+    import SearchCase from "$lib/Terapeuta/TerapeutaHome/Header/searchCase.svelte";
+    import PFP from "$lib/Terapeuta/TerapeutaHome/Header/PFP.svelte";
+    import ProfileFloatingCard from "$lib/Perfil/ProfileFloatingCard.svelte";
+    let showMenu = false;
+
+    function toggleMenu() {
+        showMenu = !showMenu;
+    }
+</script>
+
 <div class="header">
     <SearchCase />
-    <div class="pfp"><PFP /></div>
-    
+    <button class="pfp" on:click={toggleMenu}><PFP /></button>
+    {#if showMenu}
+    <div class="profile-menu">
+        <ProfileFloatingCard />
+    </div>
+    {/if}
 </div>
 
 <style>
@@ -15,10 +30,14 @@
     .pfp{
         margin-left: auto;
     }
+
+    .profile-menu{
+        position: absolute;
+        z-index: 1;
+        top: 100%;
+        right: 0;
+        margin-top: 8px;
+        margin-right: 48px;
+    }
     
 </style>
-
-<script>
-    import SearchCase from "$lib/Terapeuta/TerapeutaHome/Header/searchCase.svelte";
-    import PFP from "$lib/Terapeuta/TerapeutaHome/Header/PFP.svelte";
-</script>
