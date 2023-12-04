f4cd3f9e9e572631458adf107ee17f5eff5f0886
Vinicius <59659214+ZeAndradee@users.noreply.github.com>
2023-12-03 21:44:15 -0300

    Prontuarios andamento

---
+++ navit/src/lib/Terapeuta/Prontuarios/PacienteProntuarioInfo/PacienteProntuarioInfo.svelte
@@ -0,0 +1,72 @@
+<script>
+    let totalPacientesAtivos = "11";
+    let totalPacientesAtentidos = "3";
+    let totalPacientesAAtender = "8";
+</script>
+
+<div class="paciente-prontuario-info">
+    <div class="txt-header-prontuarios">
+        <span>Seus Prontuários</span>
+    </div>
+    <div class="cards">
+        <div class="card-data-prontuario">
+            <div class="txt-dados"><span>{totalPacientesAtivos}</span></div>
+            <div class="txt-sub"><span>Total de Pacientes Ativos</span></div>
+        </div>
+        <div class="card-data-prontuario">
+            <div class="txt-dados"><span>{totalPacientesAtentidos}</span></div>
+            <div class="txt-sub"><span>Total de Pacientes Ativos</span></div>
+        </div>
+        <div class="card-data-prontuario">
+            <div class="txt-dados"><span>{totalPacientesAAtender}</span></div>
+            <div class="txt-sub"><span>Total de Pacientes Ativos</span></div>
+        </div>
+    </div>
+</div>
+
+<style>
+
+    
+    .paciente-prontuario-info{
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+        margin-top: 33px;
+        align-items: center;
+
+    }
+    
+    .txt-header-prontuarios{
+        font-size: 20px;
+        font-weight: 700;
+        margin-right: 20px;
+    }
+    .cards{
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+        gap: 8px;
+    }
+
+    .card-data-prontuario{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        width: 250px;
+        height: max-content;
+        padding: 10px 20px;
+        background-color: white;
+        border-radius: 8px;
+        gap: 8px;
+        border: 1px solid #E0E4EC;
+    }
+    .txt-dados{
+        font-size: 18px;
+        font-weight: 700;
+    }
+
+    .txt-sub{
+        font-size: 14px;
+        font-weight: 400;
+    }
+</style>
---
+++ navit/src/lib/Terapeuta/Prontuarios/SearchBar/SearchBar.svelte
@@ -0,0 +1,34 @@
+<div class="container">
+        <form action="" class="search-bar">
+            <input type="text" placeholder="Descreva o seu problema" name="q">
+            <button type="submit">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none">
+                    <path d="M21 21L16.514 16.506L21 21ZM19 10.5C19 12.7543 18.1045 14.9163 16.5104 16.5104C14.9163 18.1045 12.7543 19 10.5 19C8.24566 19 6.08365 18.1045 4.48959 16.5104C2.89553 14.9163 2 12.7543 2 10.5C2 8.24566 2.89553 6.08365 4.48959 4.48959C6.08365 2.89553 8.24566 2 10.5 2C12.7543 2 14.9163 2.89553 16.5104 4.48959C18.1045 6.08365 19 8.24566 19 10.5V10.5Z" stroke="#656575" stroke-width="1.80206" stroke-linecap="round"/>
+                </svg> 
+            </button>
+        </form>
+</div>
+
+<style>
+    .search-bar{
+        margin-top: 45px;
+        display: flex;
+        width: max-content;
+        height: 35px;
+        background: white;
+        border-radius: 10px;
+        outline: none;
+        border: 1px solid #E0E4EC;
+    }
+
+    .search-bar button{
+        padding: 5px;
+    }
+    .search-bar input{
+        background: transparent;
+        border: 0;
+        font-family: "Inter", Arial, sans-serif;
+    }
+
+    .search-bar input:focus { outline: none; }
+</style>
